06117ad9d58331955f50c0d17b527b90666ba7a7
schau131 <35040291+schau131@users.noreply.github.com>
2020-07-28 03:26:17 +0530

    Update RateLimitFilter.java

--- RateLimiter/src/main/java/com/hom/vcs/filter/RateLimitFilter.java
+++ RateLimiter/src/main/java/com/hom/vcs/filter/RateLimitFilter.java
@@ -67,7 +67,7 @@ public class RateLimitFilter implements Filter {
 						List<ServiceConfig> serviceConfigList = entry.getValue();
 						for(ServiceConfig serviceConfig : serviceConfigList) 
 						{
-							if(clientId == null || (clientId != null && serviceConfig.getClientId().equals(clientId)))
+							if(serviceConfig.getClientId() == null || (serviceConfig.getClientId() != null && serviceConfig.getClientId().equals(clientId)))
 							{
 								ignoreRequest = this.checkForRequestValidity(serviceConfig, localDateTimes, entry.getKey());
 								checked = true;
